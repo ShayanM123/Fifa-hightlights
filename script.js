da88543b0e721a33fca5e7dc303ea37abada2da5
ShayanM123 <111317112+ShayanM123@users.noreply.github.com>
2023-01-05 16:59:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper (".swiper-container", {
+    effect: "coverflow",
+    slidesperView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        slidesShadow: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 1000,
+
+    },
+    });
